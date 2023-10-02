3dac09f86620c88023881630ad5d0248c6bb1026
vanshika27sinha <146586084+vanshika27sinha@users.noreply.github.com>
2023-10-02 22:24:12 +0530

    TASK 2

--- Words Counter.java
+++ Words Counter.java
@@ -6,7 +6,7 @@ public class CHARCOUNT extends JFrame implements ActionListener {
     JTextArea t;
     JButton button1,button2;
     CHARCOUNT(){
-        super("word counter by vanshika");
+        super("Word Counter by Vanshika");
         t=new JTextArea();
         t.setBounds(60,60,350,250);
         button1=new JButton("word");
